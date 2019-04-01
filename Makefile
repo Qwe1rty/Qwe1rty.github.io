ad11798879336ba1cf24aad4cd2d2d2a11cb91dc
Qwe1rty <caleb.rhms@gmail.com>
2019-03-31 23:24:59 -0400

    Fleshed out more the rest of the build setup

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-.PHONY: build
+.PHONY: all build run
+
+all: build run
 
 build:
 	@setup/build.sh
+
+run:
+	@cutelyst2 -r --server --app-file build/src/Release/server.dll 
--- setup/build.sh
+++ setup/build.sh
@@ -4,15 +4,16 @@ COMPILE_DIR='build'
 COMPILER_VERS='Visual Studio 15 2017 Win64'
 
 main() {
-    if [[ -L "${COMPILE_DIR}" ]] ; then
-        rm -f "$COMPILE_DIR"
-    fi
-    if [[ ! -d "${COMPILE_DIR}" ]] ; then
-        mkdir "${COMPILE_DIR}"
+    if [[ -d "${COMPILE_DIR}" ]] ; then
+        rm -rf "${COMPILE_DIR}"
     fi
 
+    mkdir "${COMPILE_DIR}"
     cd "${COMPILE_DIR}"
-    cmake .. -G "${COMPILER_VERS}" -DCMAKE_SH='CMAKE_SH-NOTFOUND' && cmake --build . --config Release
+
+    if cmake .. -G "${COMPILER_VERS}" ; then
+        cmake --build . --config Release
+    fi
 }
 
 main
